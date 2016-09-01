b7fc12161660450ade6367209374b67185e19b66
Jesse Shawl <jesse@jshawl.com>
2016-09-01 16:14:51 -0400

    sanitize

--- spec/ubbi_dubbi_spec.js
+++ spec/ubbi_dubbi_spec.js
@@ -5,13 +5,11 @@ describe("solution", function () {
     it("adds ub if the first letter is a vowel", function () {
       expect(translate("apt")).toBe("ubapt")
     })
-
     it("adds ub before the first vowel", function () {
       expect(translate("spot")).toBe("spubot")
     })
-
     it("adds ub before, only if the last letter vowel is preceded by two consonant", function () {
-      expect(translate("hello")).toBe("hubellubo")
+      expect(translate("scale")).toBe("scubale")
     })
   })
 })
